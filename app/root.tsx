5645d0095773ee40c5ac12c1cf08289f8b44a412
shiroemons <shiroemons@gmail.com>
2024-01-30 22:00:18 +0900

    prelineを追加

--- app/root.tsx
+++ app/root.tsx
@@ -7,10 +7,22 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLocation,
 } from "@remix-run/react";
+import { useEffect } from "react";
 
 import styles from "~/styles/tailwind.css"
 import algoliaStyles from "instantsearch.css/themes/algolia-min.css";
+import { type IStaticMethods } from "preline/preline";
+declare global {
+  interface Window {
+    HSStaticMethods: IStaticMethods;
+  }
+}
+if (typeof window !== "undefined") {
+  require("preline/preline");
+}
+
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
@@ -19,6 +31,12 @@ export const links: LinksFunction = () => [
 ];
 
 export default function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.HSStaticMethods.autoInit();
+  }, [location.pathname]);
+
   return (
     <html lang="ja">
       <head>
